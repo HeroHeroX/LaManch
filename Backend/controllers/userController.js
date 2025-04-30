9c10fd1baf335b4059bb902de042206a0b05533e
LEE <lecongduy70@gmail.com>
2025-04-30 11:53:05 +0900

    Login api

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+//User Login
+const loginUser = async (req,res) => {
+
+}
+
+//User register
+const registerUser = async (req,res) => {
+    res.json({msg:'User registered successfully'})
+}
+
+
+//Admin Login
+const adminLogin = async (req,res) => {
+
+}
+export {loginUser, registerUser, adminLogin}
