fef20792fefccc5d4b7038825764bc61145b5af7
LEE <lecongduy70@gmail.com>
2025-05-13 09:28:35 +0900

    fix file

--- Frontend/src/pages/Collection.jsx
+++ Frontend/src/pages/Collection.jsx
@@ -2,7 +2,7 @@ import React, { useContext,useEffect,useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
-import Productitem from '../components/ProductItem';
+import Productitem from '../components/Productitem';
 
 const Collection = () => {
 
