76de9fc9c46834f8e43844b175ed2706512a3853
HeroHeroX <129927514+HeroHeroX@users.noreply.github.com>
2025-11-17 14:16:41 +0900

    Update newsletter subscription message

--- Frontend/src/components/NewsletterBox.jsx
+++ Frontend/src/components/NewsletterBox.jsx
@@ -9,7 +9,7 @@ const NewsletterBox = () => {
   return (
     <div className='text-center bg-[#F8F9FA] pb-10'>
       <p className='text-[25px] font-bold text-gray-800'>Subscribe now & get 20% off</p>
-      <p className='text-gray-500 mt-3 text-[20px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p className='text-gray-500 mt-3 text-[20px]'>Sign up today to unlock timeless styles, curated collections, and members‑only deals..</p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex mx-auto gap-3 my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
         <button type='submit' className='bg-black text-white text-xs px-10 py-4 cursor-pointer'>SUBSCRIBE</button>
