02746ec2e6f900251cbbae0a8ffdbd672861b3b7
HeroHeroX <129927514+HeroHeroX@users.noreply.github.com>
2025-11-21 18:22:55 +0900

    test

--- Frontend/src/components/BestSeller.jsx
+++ Frontend/src/components/BestSeller.jsx
@@ -17,9 +17,9 @@ const BestSeller = () => {
   return (
     <div className='px-4 py-5 bg-[#DFDFDC]'>
       <div className='text-center py-8 text-[35px]'>
-        <Title text1={'BEST'} text2={'SELLER'}/>
+        <Title text1={'人気'} text2={'アイテム'}/>
         <p className='w-3/4 m-auto text-[15px] sm:text-lg md:text-xl text-gray-600'>
-            Our best sellers combine style and comfort, making them everyday favorites.
+            人気アイテムはスタイルと快適さを両立し、毎日着たくなるお気に入りです
         </p>
       </div>
 
